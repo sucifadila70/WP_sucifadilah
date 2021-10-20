6ea83e40904681f262db1369e8fd8cb5076a0fbf
sucifadila70 <91003090+sucifadila70@users.noreply.github.com>
2021-10-21 00:53:00 +0700

    Add files via upload

---
+++ View-latihan1.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+    <tittle>Latihan 1</tittle>
+</head>
+<body>
+    Hallo kawan.. yuk kita belajar web Programming..!!!<br>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
