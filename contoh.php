2521777cfe8dfdae6051a694336f351e7b8fc210
sucifadila70 <91003090+sucifadila70@users.noreply.github.com>
2021-10-21 01:10:47 +0700

    Add files via upload

---
+++ contoh.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh extends CI_Controller
+{
+    public function index()
+  {
+        echo "<h1>Perkenalkan</>";
+        echo"nama saya Suci fadilah
+            saya tinggal di daerah kapuk
+            hobby yang saya sukai adalah
+            menonton film";
+  }
+}  
